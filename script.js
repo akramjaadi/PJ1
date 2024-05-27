30e0120baaaae3f9f874b2ad00a1c67c87440242
Akram <iak115376@gmail.com>
2024-05-27 15:56:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Akram");
+alert("salam");
+A=prompt("saisir note anglais: ")
+B=prompt("saisir note Communication: ")
+C=prompt("saisir note Programation: ")
+D=prompt("saisir note Web: ")
+E=prompt("saisir note Bureautique: ")
+alert("la moyen est: "+(A*2+B*3+C*4+D*3+E*2))/14
